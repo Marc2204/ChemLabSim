2512253342d7fd76553520e500195b74c35713cb
marc <70189311+Marc2204@users.noreply.github.com>
2025-02-08 20:46:51 +0800

    Nothing

--- chemlab/src/components/LabTable.js
+++ chemlab/src/components/LabTable.js
@@ -5,12 +5,11 @@ import chemGreen from '../imageButton/chemGreen.png';
 import chemRed from '../imageButton/chemRed.png';
 import chemBlue from '../imageButton/chemBlue.png';
 import trash from '../imageButton/trash.webp';
+import styles from "../componentDesign/LabTable.module.css";
 
 const LabTable = () => {
   const [inputValue, setInputValue] = useState("");
 
-
-  // Para sa tatlong mukhang panturok
   const items = [
     { src: chemYellow, alt: "chemYellow", id: "p2-tips" },
     { src: chemGreen, alt: "chemYellow", id: "p200-tips" },
@@ -19,15 +18,13 @@ const LabTable = () => {
     { src: trash, alt: "chemYellow", id: "trash", isTrash: true },
   ];
 
-  // Handle item click to update the input value
   const handleItemClick = (itemName) => {
     setInputValue(itemName);
   };
 
   return (
-    <div className="lab-container">
-      <h1>CHEMISTRY LABORATORY SIMULATION</h1>
-      <div className="shelf">
+    <>
+      <div className={styles.shelf}>
         {items.slice(0, 4).map((item) => (
           <ItemImage
             key={item.id}
@@ -39,31 +36,27 @@ const LabTable = () => {
           />
         ))}
       </div>
-      <div className="table">
-        {items.slice(4).map((item) => (
-          <ItemImage
-            key={item.id}
-            src={item.src}
-            alt={item.alt}
-            id={item.id}
-            isTrash={item.isTrash}
-            onItemClick={handleItemClick}
-          />
-        ))}
+
+      <div className={styles.table}>
+        <button 
+          type="button" 
+          className={styles.trashButton} 
+          onClick={() => handleItemClick("trash")}
+        >
+          <img src={trash} alt="Trash" className={styles.trashImage} />
+        </button>
       </div>
 
-        // text screen
-      <div className="chem-screen">
+      <div className={styles.chemScreen}>
         <input type="text" value={inputValue} disabled />
       </div>
 
-      // Buttons for chems
-      <div className="chemical-button">
+      <div className={styles.chemicalButton}>
         <button onClick={() => setInputValue("chem1")}>chem1</button>
         <button onClick={() => setInputValue("chem2")}>chem2</button>
         <button onClick={() => setInputValue("chem3")}>ADD</button>
       </div>
-    </div>
+    </>
   );
 };
 
